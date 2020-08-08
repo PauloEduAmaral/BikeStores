64f41cf2e47c681b1b4fe0bccab800ecc414cfa5
PauloEduAmaral <paulo.e.amaral@gmail.com>
2020-08-08 18:02:59 -0300

    Visual Studio initial template files for Web Application.

---
+++ BikeStores/Data/BikeStoresContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace BikeStores.Models
+{
+    public class BikeStoresContext : DbContext
+    {
+        public BikeStoresContext (DbContextOptions<BikeStoresContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Brand> Brand { get; set; }
+    }
+}
